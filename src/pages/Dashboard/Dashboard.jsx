cdeaa4be8f8d86f7e329e6c95bf7772cbbef7582
Abhinav <abhinavke091@gmail.com>
2024-02-28 12:49:32 +0530

    leftnav-ui

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
 import './Dashboard.scss'
+import { Outlet } from 'react-router-dom'
+import LeftNav from '../../components/LeftNav/LeftNav'
 
 const Dashboard = () => {
   return (
-    <div>Dashboard</div>
+    <div className="dashboard">
+      <div className="left-flex">
+        <LeftNav/>
+      </div>
+      <div className="right-flex">
+        <Outlet/>
+      </div>
+    </div>
   )
 }
 
