72f531dd828160f3749235f4c0422bc85a149bb6
Abhinav <abhinavke091@gmail.com>
2024-02-28 15:22:26 +0530

    card-ui

--- src/pages/ProductsPage/ProductsPage.jsx
+++ src/pages/ProductsPage/ProductsPage.jsx
@@ -1,13 +1,21 @@
-import React from 'react'
-import './ProductPage.scss'
-import TopNav from '../../components/Dashboard-component/TopNav/TopNav'
+import React from 'react';
+import './ProductPage.scss';
+import TopNav from '../../components/Dashboard-component/TopNav/TopNav';
+import Card from '../../components/Card/Card';
 
 const ProductsPage = () => {
   return (
     <div className='products-page'>
-      <TopNav/>
+      <TopNav />
+      <div className='product-cards'>
+        <Card/>
+        <Card/>
+        <Card/>
+        <Card/>
+        <Card/>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default ProductsPage
+export default ProductsPage;
