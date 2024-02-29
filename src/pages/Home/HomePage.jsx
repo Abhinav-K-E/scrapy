4cd901bdebf98ca66d5871ccf414ebb55a1c6853
Abhinav <abhinavke091@gmail.com>
2024-02-29 09:49:21 +0530

    login-btn-added

--- src/pages/Home/HomePage.jsx
+++ src/pages/Home/HomePage.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
 import './HomePage.scss'
+import { Link } from 'react-router-dom'
 
 const HomePage = () => {
   return (
-    <div>HomePage</div>
+    <div className='home'>
+      <Link to='/dashboard/products' className="login-btn">
+        Login to Continue
+      </Link>
+    </div>
   )
 }
 
