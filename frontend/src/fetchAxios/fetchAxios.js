c614aa2c6dd403bcd11b2ca905087578950589eb
Abhinav K E <109943270+Abhinav-K-E@users.noreply.github.com>
2025-02-18 21:47:41 +0530

    backend hosted

--- frontend/src/fetchAxios/fetchAxios.js
+++ frontend/src/fetchAxios/fetchAxios.js
@@ -1,7 +1,8 @@
-import axios from 'axios';
+import axios from "axios";
 
 const fetchAxios = axios.create({
-  baseURL: 'http://localhost:5001/',
+  // baseURL: 'http://localhost:5001/',
+  baseURL: "https://scrapy-gwx3.vercel.app/",
 });
 
 export default fetchAxios;
